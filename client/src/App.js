7fc32cd478e5d593c699f31de0ec14f895772837
SriyaCodes <babysriya1193@gmail.com>
2025-07-12 22:48:55 +0530

    create client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-blue-600 font-bold p-6">
+      Tailwind is working! 🌸
+    </div>
+  );
+}
+
+
+export default App;
